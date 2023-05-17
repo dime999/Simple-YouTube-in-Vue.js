3d366e1c1c0b58e04c9f2d9a8315c22ec3790b19
Adem Halilović <halilovicadem@gmail.com>
2023-05-17 08:38:04 +0200

    Adding styling

--- video-browser/src/components/VideoListItem.vue
+++ video-browser/src/components/VideoListItem.vue
@@ -1,7 +1,9 @@
 <template>
-<li class="list-group-item">
-    <img :src="thumbnailUrl" />
+<li class="list-group-item media">
+    <img class="mr-3" :src="thumbnailUrl" />
+    <div class="media-body">
     {{ video.snippet.title }}
+</div>
 </li>
 </template>
 
@@ -14,6 +16,15 @@ export default{
             return this.video.snippet.thumbnails.default.url;
         }
     }
-    
 };
 </script>
+
+<style scoped>
+li{
+    display: flex;
+    cursor: pointer;
+}
+li:hover{
+    background-color: #eee;
+}
+</style>
